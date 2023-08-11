9b65dbb9a6e682bdd87398d8db9b24e5e797cdc5
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-12 00:25:32 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,26 +9,25 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    printf("Last digit of %d is %d and is ", n, n % 10);
+	printf("Last digit of %d is %d and is ", n, n % 10);
 
-    if (n % 10 > 5)
-    {
-        printf("greater than 5\n");
-    }
-    else if (n % 10 == 0)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        printf("less than 6 and not 0\n");
-    }
+	if (n % 10 > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
 
-    return (0);
+	return (0);
 }
-

82c3db541d19e91f6639fd2a29f1246e3708b4fd
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 01:11:03 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks if a character is an alphabetic letter.
- * @c: The character to be checked.
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
  *
- * Return: 1 if c is an alphabetic letter, 0 otherwise.
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return 1;
-    else
-        return 0;
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }

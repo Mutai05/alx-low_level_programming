555f35f6abf50cefab124dc3268478188288f4b2
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 01:39:15 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n: The number from which to extract and print the last digit
+ * @n: The number to extract the last digit from
  *
- * Return: The value of the last digit.
+ * Return: Value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+	int last_digit;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (n < 0)
+		n = -n;
 
-    _putchar(last_digit + '0');
-
-    return last_digit;
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }

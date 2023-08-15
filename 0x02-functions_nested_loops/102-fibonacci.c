f55e754102be0cc87597a66cdb05a88891752b8d
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 02:53:52 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,20 @@
  */
 int main(void)
 {
-    int i;
-    long fib1 = 1, fib2 = 2, next;
+	int i;
+	long fib1 = 1, fib2 = 2, next;
 
-    printf("%ld, %ld", fib1, fib2);
+	printf("%ld, %ld", fib1, fib2);
 
-    for (i = 2; i < 50; i++)
-    {
-        next = fib1 + fib2;
-        printf(", %ld", next);
-        fib1 = fib2;
-        fib2 = next;
-    }
+	for (i = 2; i < 50; i++)
+	{
+		next = fib1 + fib2;
+		printf(", %ld", next);
+		fib1 = fib2;
+		fib2 = next;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }

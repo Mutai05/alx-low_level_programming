4bf49835393859fdca9200181f9300f5dac2418c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 01:58:19 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
+ * jack_bauer - Prints every minute of a day
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar((hour / 10) + '0');
-            _putchar((hour % 10) + '0');
-            _putchar(':');
-            _putchar((minute / 10) + '0');
-            _putchar((minute % 10) + '0');
-            _putchar('\n');
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
 }

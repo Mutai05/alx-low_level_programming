72ad03c64c6458351cc4779f60ed9aa85b95207e
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 02:24:15 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,26 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_to_98 - funtion to print to 98 in ascending r descending order
- *
- * @n: n of type integer
+ * print_to_98 - function to print numbers to 98 in ascending or descending order
+ * @n: input integer
  */
-
 void print_to_98(int n)
 {
-	int num;
+    int num;
 
-	if (n <= 98)
-	{
-		for (num = n; num < 98; num++)
-			printf("%d, ", num);
-	}
-	else
-	{
-		for (num = n; num > 98; num--)
-			printf("%d, ", num);
-	}
-	printf("98\n");
+    if (n <= 98)
+    {
+        for (num = n; num < 98; num++)
+        {
+            printf("%d, ", num);
+        }
+    }
+    else
+    {
+        for (num = n; num > 98; num--)
+        {
+            printf("%d, ", num);
+        }
+    }
+    printf("98\n");
 }

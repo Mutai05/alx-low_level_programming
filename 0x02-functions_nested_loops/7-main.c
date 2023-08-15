cc0182e8d881fa0c30b2898ac05238e7b4b78010
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 01:46:26 +0300

    Update 7-main.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -9,21 +9,10 @@ int main(void)
 {
     int r;
 
-    r = print_last_digit(98);
-    _putchar('0' + r);  /* Print the last digit */
-    _putchar('\n');
-    
-    r = print_last_digit(0);
-    _putchar('0' + r);  /* Print the last digit */
-    _putchar('\n');
-
+    print_last_digit(98);
+    print_last_digit(0);
     r = print_last_digit(-1024);
-    _putchar('0' + r);  /* Print the last digit */
+    _putchar('0' + r);
     _putchar('\n');
-    
-    r = print_last_digit(INT_MIN);  /* Using INT_MIN */
-    _putchar('0' + (r * (r != -1)));  /* Print the last digit, handling INT_MIN case */
-    _putchar('\n');
-    
     return (0);
 }

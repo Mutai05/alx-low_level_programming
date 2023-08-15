0fc2214b5ed57dfb84636e290a692d9600632705
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 02:43:53 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - prints the n times table
  *
- * @n: integer value specifying the times table
+ * @n: n of type integer
  */
 void print_times_table(int n)
 {
+	if (n < 0 || n > 15)
+		return;
+
 	int row, column, result;
 
-	if (n >= 0 && n <= 15)
+	for (row = 0; row <= n; row++)
 	{
-		for (row = 0; row <= n; row++)
+		for (column = 0; column <= n; column++)
 		{
-			for (column = 0; column <= n; column++)
+			result = row * column;
+
+			if (column != 0)
+				_putchar(',');
+
+			if (result < 10)
+				_putchar(' ');
+			else if (result < 100)
+				_putchar(' ');
+
+			if (result >= 100)
 			{
-				result = row * column;
-
-				if (column != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-
-					if (result < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (result < 100)
-					{
-						_putchar(' ');
-					}
-				}
-				if (result < 10)
-					_putchar(result + '0');
-				else if (result < 100)
-				{
-					_putchar(result / 10 + '0');
-					_putchar(result % 10 + '0');
-				}
-				else
-				{
-					_putchar(result / 100 + '0');
-					_putchar((result / 10) % 10 + '0');
-					_putchar(result % 10 + '0');
-				}
+				_putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
 			}
-			_putchar('\n');
+			else if (result >= 10)
+			{
+				_putchar(result / 10 + '0');
+			}
+
+			_putchar(result % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }

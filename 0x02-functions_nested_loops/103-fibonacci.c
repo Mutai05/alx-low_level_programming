a16327a7f7db0351c17f848f820732f7ac703b8c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-16 03:05:59 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,23 +10,23 @@
  */
 int main(void)
 {
-    long sum = 2; /* Initialize sum with the value of the first even term (2) */
-    long a = 1;
-    long b = 2;
-    long c = a + b;
+	long sum = 2; /* Initialize sum with the value of the first even term (2) */
+	long a = 1;
+	long b = 2;
+	long c = a + b;
 
-    while (c <= 4000000)
-    {
-        if (c % 2 == 0)
-        {
-            sum += c;
-        }
+	while (c <= 4000000)
+	{
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
 
-        a = b;
-        b = c;
-        c = a + b;
-    }
+		a = b;
+		b = c;
+		c = a + b;
+	}
 
-    printf("%ld\n", sum);
-    return (0);
+	printf("%ld\n", sum);
+	return (0);
 }

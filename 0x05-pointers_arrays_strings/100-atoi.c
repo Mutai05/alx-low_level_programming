f188279dfa3afe34edfd9c349d3db657f2be391c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-22 02:15:38 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,23 @@
-#include "main.h"
-
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
+ * _atoi - converts a string to an integer
  *
- * Return: The integer value of the converted string.
+ * @s: string input parameter
+ *
+ * Return: converted integer from string
  */
-
 int _atoi(char *s)
 {
-    unsigned int num = 0;
-    int sign = 1;
+	int num = 0;
+	int sign = 1;
 
-    do {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-            num = (num * 10) + (*s - '0');
-        else if (num > 0)
-            break;
-    } while (*s++);
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-    return (num * sign);
+	return (num * sign);
 }

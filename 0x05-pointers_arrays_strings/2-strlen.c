85a8a570bf780303fec9c0b8df921b699d6b321e
Mutai Kelvin <Mutai05>
2023-08-21 14:29:35 -0700

    -Write a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 /* Function prototype for reset_to_98 */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /* MAIN_H */
 

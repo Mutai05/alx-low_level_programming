ce5355b0a1625aeba9a68419928736c0be09e3b9
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-22 01:43:54 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,18 +4,15 @@
  * puts2 - Prints every other character of a string.
  * @str: The string to be printed.
  */
+
 void puts2(char *str)
 {
-    int i = 0;
+	int i;
 
-    /* Loop through the string, incrementing by 2 each time */
-    while (str[i] != '\0')
-    {
-        /* Print the current character */
-        _putchar(str[i]);
-        i += 2;
-    }
-
-    /* Print a new line character */
-    _putchar('\n');
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
 }

efd05149bf1f53ecdc1fcc31b804959c62fbd006
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-22 01:44:45 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,19 @@
 
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	for (i = 0; str[i] != '\0'; ++i)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		len++;
 	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }

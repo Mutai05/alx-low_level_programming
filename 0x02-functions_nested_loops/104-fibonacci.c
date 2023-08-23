3b328eafab6c643fcf4227ab443e87a51028d53f
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-23 21:46:49 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,38 +7,38 @@
  */
 int main(void)
 {
-    int i;
-    unsigned long int a = 1, b = 2, c;
-    unsigned long int a1, a2, b1, b2, c1, c2;
+	int i;
+	unsigned long int a = 1, b = 2, c;
+	unsigned long int a1, a2, b1, b2, c1, c2;
 
-    printf("%lu, %lu", a, b);
-    for (i = 0; i < 96; i++)
-    {
-        if (i < 90)
-        {
-            c = a + b;
-            printf(", %lu", c);
-            a = b;
-            b = c;
-        }
-        else
-        {
-            if (i == 90)
-            {
-                a1 = a / 1000000000;
-                a2 = a % 1000000000;
-                b1 = b / 1000000000;
-                b2 = b % 1000000000;
-            }
-            c1 = a1 + b1 + ((a2 + b2) / 1000000000);
-            c2 = (a2 + b2) % 1000000000;
-            printf(", %lu%lu", c1, c2);
-            a1 = b1;
-            a2 = b2;
-            b1 = c1;
-            b2 = c2;
-        }
-    }
-    printf("\n");
-    return (0);
+	printf("%lu, %lu", a, b);
+	for (i = 0; i < 96; i++)
+	{
+		if (i < 90)
+		{
+			c = a + b;
+			printf(", %lu", c);
+			a = b;
+			b = c;
+		}
+		else
+		{
+			if (i == 90)
+			{
+				a1 = a / 1000000000;
+				a2 = a % 1000000000;
+				b1 = b / 1000000000;
+				b2 = b % 1000000000;
+			}
+			c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+			c2 = (a2 + b2) % 1000000000;
+			printf(", %lu%lu", c1, c2);
+			a1 = b1;
+			a2 = b2;
+			b1 = c1;
+			b2 = c2;
+		}
+	}
+	printf("\n");
+	return (0);
 }

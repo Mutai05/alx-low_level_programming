f427acf696515ec417d66446b56b2312569acf03
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-23 21:32:10 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,23 +3,22 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - generates random passwords
+ * Return: 0
  */
+
 int main(void)
 {
-    char password[15];
-    int i;
+	int sum;
+	char c;
 
-    srand(time(NULL));
-
-    for (i = 0; i < 14; i++)
-    {
-        password[i] = rand() % 94 + 33;  /* Generates a random character between ASCII 33 and 126 */
-    }
-    password[i] = '\0';  /* Null-terminate the string */
-
-    printf("Tada! Congrats\n%s\n", password);
-    return (0);
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }

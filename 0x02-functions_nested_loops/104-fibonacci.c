78fdbd2db6394338b25966c56efc6c719fe0309b
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-23 21:44:09 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the first 98 Fibonacci numbers
  *
- * Description: Computes and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2.
- *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 2;
-	unsigned long c;
-	int count;
+    int i;
+    unsigned long int a = 1, b = 2, c;
+    unsigned long int a1, a2, b1, b2, c1, c2;
 
-	printf("%lu, %lu", a, b);
-
-	for (count = 2; count < 98; count++)
-	{
-		c = a + b;
-		printf(", %lu", c);
-		a = b;
-		b = c;
-	}
-
-	printf("\n");
-	return (0);
+    printf("%lu, %lu", a, b);
+    for (i = 0; i < 96; i++)
+    {
+        if (i < 90)
+        {
+            c = a + b;
+            printf(", %lu", c);
+            a = b;
+            b = c;
+        }
+        else
+        {
+            if (i == 90)
+            {
+                a1 = a / 1000000000;
+                a2 = a % 1000000000;
+                b1 = b / 1000000000;
+                b2 = b % 1000000000;
+            }
+            c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+            c2 = (a2 + b2) % 1000000000;
+            printf(", %lu%lu", c1, c2);
+            a1 = b1;
+            a2 = b2;
+            b1 = c1;
+            b2 = c2;
+        }
+    }
+    printf("\n");
+    return (0);
 }

9e88cec351e54ac099c104ac76c293b213954b4a
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 05:30:51 +0300

    Update 101-print_number.c
    Betty coding style

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,22 +4,19 @@
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
+
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
 
-	if (n / 10 != 0)
-		print_number(n / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }

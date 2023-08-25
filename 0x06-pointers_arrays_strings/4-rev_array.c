822680ab95e253666d854bdf34cdb958b7db27a4
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 04:56:41 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,14 +3,15 @@
  * @a: The array of integers.
  * @n: The number of elements in the array.
  */
+
 void reverse_array(int *a, int n)
 {
-    int i, temp;
+	int i, temp;
 
-    for (i = 0; i < n / 2; i++)
-    {
-        temp = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = temp;
-    }
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
 }

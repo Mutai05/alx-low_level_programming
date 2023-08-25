e2be0a4d4c6a7801b10a9b07c89764971117b06c
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 05:04:16 +0300

    Update 5-string_toupper.c
    Betty coding style format

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,18 +4,16 @@
  *
  * Return: A pointer to the modified string.
  */
-char *string_toupper(char *str)
+
+char *string_toupper(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
-        }
-        i++;
-    }
-
-    return (str);
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
 }

48c24ae928b0f36bd62413a3816aa4a964a15342
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 05:58:04 +0300

    Update 104-print_buffer.c
    Betty coding style

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,45 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer.
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
  * @b: The buffer to be printed.
- * @size: The size of the buffer.
+ * @size: The number of bytes to be printed from the buffer.
  */
+
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int byte, index;
 
-	if (size <= 0)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("\n");
-		return;
-	}
+		printf("%08x: ", byte);
 
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
-
-		for (j = i; j < i + 10; j++)
+		for (index = 0; index < 10; index++)
 		{
-			if (j < size)
-				printf("%02x", b[j]);
-			else
+			if ((index + byte) >= size)
 				printf("  ");
-
-			if (j % 2 != 0)
-				printf(" ");
-		}
-
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-			{
-				if (b[j] >= 32 && b[j] <= 126)
-					printf("%c", b[j]);
-				else
-					printf(".");
-			}
 			else
-			{
+				printf("%02x", *(b + index + byte));
+			if ((index % 2) != 0 && index != 0)
 				printf(" ");
-			}
 		}
-
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
 		printf("\n");
 	}
+	if (size <= 0)
+		printf("\n");
 }

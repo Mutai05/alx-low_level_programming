a587bdc1a8d8c7dea8260932fefbcf4ea60c1b88
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 04:49:54 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,14 +6,14 @@
  * Return: 0 if s1 and s2 are equal, a negative value if s1 is less than s2,
  *         or a positive value if s1 is greater than s2
  */
+
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	while ((*s1 && *s2) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-    {
-        i++;
-    }
-
-    return s1[i] - s2[i];
+	return (*s1 - *s2);
 }

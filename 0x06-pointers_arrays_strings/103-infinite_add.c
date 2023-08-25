74ef7309d8ddc995709df03573e4ea479cf433fb
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 05:51:00 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,61 +1,49 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * infinite_add - Adds two numbers as strings and stores the result in a buffer
- * @n1: The first number as a string
- * @n2: The second number as a string
- * @r: The buffer to store the result
- * @size_r: The size of the buffer
+ * infinite_add - adds two numbers.
+ * @n1: First number
+ * @n2: Second number
+ * @r: result buffer
+ * @size_r: result buffer size
  *
- * Return: A pointer to the result, or 0 if the result can't be stored in r.
+ * Return: 0 if the result can not be stored in r,
+ * otherwise a pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum, i, j;
+	int i, j, k, l, m, n;
 
-	/* Calculate the lengths of the input strings */
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-
-	/* Ensure the buffer size is sufficient */
-	if (len1 + 1 > size_r || len2 + 1 > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-
-	/* Start adding digits from the end of both numbers */
-	i = len1 - 1;
-	j = len2 - 1;
-	r[size_r] = '\0'; /* Null-terminate the result string */
-
-	while (i >= 0 || j >= 0 || carry)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		/* Calculate the sum of digits at the current positions */
-		sum = carry;
+		n = m;
 		if (i >= 0)
-			sum += n1[i] - '0';
+			n += n1[i] - '0';
 		if (j >= 0)
-			sum += n2[j] - '0';
-
-		/* Calculate the carry for the next iteration */
-		carry = sum / 10;
-
-		/* Store the digit in the result buffer */
-		r[size_r - 1] = (sum % 10) + '0';
-
-		/* Move to the previous positions in the input and result */
-		i--;
-		j--;
-		size_r--;
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-
-	/* If there's still a carry, the result can't fit in the buffer */
-	if (carry)
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
 		return (0);
-
-	/* Shift the result to the beginning of the buffer */
-	while (*r == '0')
-		r++;
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
 
 	return (r);
 }

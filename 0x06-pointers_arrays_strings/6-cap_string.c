1c5434f6c1e40c063453d417675fa8e98efe5a2d
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 05:12:29 +0300

    Update 6-cap_string.c
    Betty coding style

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,31 @@
-/**
- * cap_string - funtion that capital string
- * @s: Address of the string
- * Return: The uppercase @s address
- */
+#include "main.h"
 
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
+ */
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + i))
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if ((s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
-				s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' ||
-				s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' ||
-				s[i - 1] == '{' || s[i - 1] == '}'))
-				s[i] -= 32;
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
+		i++;
 	}
 	return (s);
 }

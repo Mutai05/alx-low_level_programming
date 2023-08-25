f1df5f86fe266f7426bdba2ac597cd486083493a
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-25 04:37:30 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,22 +8,20 @@
  *
  * Return: a pointer to the resulting string dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0;
-    int i;
+	int i, j;
 
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
 
-    dest[dest_len + i] = '\0';
-
-    return dest;
+	return (dest);
 }

a80db0d71e7bcad916a1d95cb5623c5f189fa077
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-29 01:19:35 +0300

    Update 2-strchr.c
    Betty coding style

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,18 @@
 
 char *_strchr(char *s, char c)
 {
-    int a;
+		int a;
 
-    while (1)
-    {
-        a = *s++;
-        if (a == c)
-        {
-            return (s - 1);
-        }
-        if (a == 0)
-        {
-            return (NULL);
-        }
-    }
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }

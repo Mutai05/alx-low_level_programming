b40d121051dc75a95c6acb192fc6577d43ce0cd6
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-29 01:40:51 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,20 +9,16 @@
  * Return: a pointer to the first occurrence of the character
  * c in the string s, or NULL if the character is not found
  */
+
 char *_strchr(char *s, char c)
 {
-    int a;
-
-    while (1)
-    {
-        a = *s++;
-        if (a == c)
-        {
-            return (s - 1);
-        }
-        if (a == 0)
-        {
-            return (NULL);
-        }
-    }
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return ('\0');
 }

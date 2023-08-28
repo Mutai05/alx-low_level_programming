bff003fa9b4463132c63a8845af67342e4dbbcba
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-29 01:22:31 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-		int a;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+	return ('\0');
 }

2497e864644f3a2624866144b259ee06b8212be4
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-29 02:10:36 +0300

    Update 7-print_chessboard.c
    Betty coding style

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,16 +5,17 @@
  * print_chessboard - prints the chessboard
  * @a: 2D array representing the chessboard
  */
+
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            putchar(a[i][j]);
-        }
-        putchar('\n');
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }

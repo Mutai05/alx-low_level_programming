c5b7895d023bb2e407bf17b7782e641550db6968
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-29 01:10:30 +0300

    Update 1-memcpy.c
    Betty coding style.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,15 @@
  *
  * Return: a pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    return (dest);
+	return (dest);
 }

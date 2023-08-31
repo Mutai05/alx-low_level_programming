728ec477c7304935fef1c7119e87667db6d267d3
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-31 04:04:18 +0300

    Update 1-print_rev_recursion.c
    Fix Betty coding style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,13 +5,13 @@
  * @s: The string to be printed in reverse.
  *
  * Description: This function uses recursion to print a string
- *              in reverse order.
+ * in reverse order.
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') /* Base case: End of the string */
-        return;
+	if (*s == '\0') /* Base case: End of the string */
+		return;
 
-    _print_rev_recursion(s + 1); /* Recurse for the next character */
-    _putchar(*s); /* Print the current character */
+	_print_rev_recursion(s + 1); /* Recurse for the next character */
+	_putchar(*s); /* Print the current character */
 }

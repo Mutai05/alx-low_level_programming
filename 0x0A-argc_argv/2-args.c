e76bbaa1a1c64eb15d30f8ece93b67f10cad8975
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-09-02 04:46:35 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,12 @@
  * @argv: Array of command-line argument strings
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	int i;
 
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }

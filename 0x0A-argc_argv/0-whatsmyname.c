605f9f6d40943ea9603c7c7cfb4ed3b82816481c
Mutai Kelvin <Mutai05>
2023-09-01 18:27:29 -0700

    Fix betty coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,10 @@
  */
 int main(int argc, char *argv[])
 {
-    if (argc > 0)
-    {
-        printf("%s\n", argv[0]);
-    }
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 
-    return (0);
+	return (0);
 }

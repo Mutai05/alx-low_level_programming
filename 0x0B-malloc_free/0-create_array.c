fb2243a69d2bd26e7ce05d3387b02c4c1118b897
Mutai Kelvin <Mutai05>
2023-09-05 14:53:53 -0700

    Fix Betty coding style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of characters and initializes it with a character
+ * create_array - Creates an array of characters and initialize
  * @size: The size of the array
  * @c: The character used for initialization
  *
@@ -9,24 +9,24 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+	char *array;
+	unsigned int i;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    array = (char *)malloc(size * sizeof(char));
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+	array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        array[i] = c;
-    }
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 
-    return (array);
+	return (array);
 }

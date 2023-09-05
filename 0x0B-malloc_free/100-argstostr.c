412b9323409e4e4ee5170662a3891c288cde74a3
Mutai Kelvin <Mutai05>
2023-09-05 15:38:54 -0700

    Write a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,39 +10,39 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, k, len;
-    char *s;
+	int i, j, k, len;
+	char *s;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    /* calculate the length of the new string */
-    len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            len++;
-        len++; /* for the newline character */
-    }
-    len++; /* for the null terminator */
+	/* calculate the length of the new string */
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++; /* for the newline character */
+	}
+	len++; /* for the null terminator */
 
-    /* allocate memory for the new string */
-    s = malloc(sizeof(char) * len);
-    if (s == NULL)
-        return (NULL);
+	/* allocate memory for the new string */
+	s = malloc(sizeof(char) * len);
+	if (s == NULL)
+		return (NULL);
 
-    /* copy the arguments to the new string */
-    k = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            s[k] = av[i][j];
-            k++;
-        }
-        s[k] = '\n';
-        k++;
-    }
-    s[k] = '\0';
-    return (s);
+	/* copy the arguments to the new string */
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
 }

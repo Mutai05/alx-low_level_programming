deb83a49990d95be3cbcdee19f80337a30ce7646
Mutai Kelvin <Mutai05>
2023-09-05 14:59:52 -0700

    Fix betty coding style

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,28 +8,28 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    int length = 0;
-    int i;
+	char *duplicate;
+	int length = 0;
+	int i;
 
-    /* Check if str is NULL */
-    if (str == NULL)
-        return (NULL);
+	/* Check if str is NULL */
+	if (str == NULL)
+		return (NULL);
 
-    /* Calculate the length of the input string */
-    while (str[length] != '\0')
-        length++;
+	/* Calculate the length of the input string */
+	while (str[length] != '\0')
+		length++;
 
-    /* Allocate memory for the duplicated string */
-    duplicate = (char *)malloc((length + 1) * sizeof(char));
+	/* Allocate memory for the duplicated string */
+	duplicate = (char *)malloc((length + 1) * sizeof(char));
 
-    /* Check if memory allocation was successful */
-    if (duplicate == NULL)
-        return (NULL);
+	/* Check if memory allocation was successful */
+	if (duplicate == NULL)
+		return (NULL);
 
-    /* Copy the input string to the duplicated string */
-    for (i = 0; i <= length; i++)
-        duplicate[i] = str[i];
+	/* Copy the input string to the duplicated string */
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
 
-    return (duplicate);
+	return (duplicate);
 }

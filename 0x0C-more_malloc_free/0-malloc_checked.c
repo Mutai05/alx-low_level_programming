f6a3b541680090c19a3a84f0cf284ba2df197edf
Mutai Kelvin <Mutai05>
2023-09-07 15:50:07 -0700

    Fixed Aunty Betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,21 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: the size of memory to allocate
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *ptr;
 
-	p = malloc(b);
-	if (p == NULL)
-	{
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
-	}
-	return (p);
+
+	return (ptr);
 }
 

c78116e4a1d4d913f98d74267b73730a267675e3
Mutai Kelvin <Mutai05>
2023-09-07 16:37:11 -0700

    Fix betty coding style

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,30 +11,30 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *new_ptr, *old_ptr;
-    unsigned int i;
+	char *new_ptr, *old_ptr;
+	unsigned int i;
 
-    if (new_size == old_size)
-        return (ptr);
+	if (new_size == old_size)
+		return (ptr);
 
-    if (ptr == NULL)
-        return (malloc(new_size));
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    old_ptr = ptr;
-    new_ptr = malloc(new_size);
+	old_ptr = ptr;
+	new_ptr = malloc(new_size);
 
-    if (new_ptr == NULL)
-        return (NULL);
+	if (new_ptr == NULL)
+		return (NULL);
 
-    for (i = 0; i < old_size && i < new_size; i++)
-        new_ptr[i] = old_ptr[i];
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_ptr[i] = old_ptr[i];
 
-    free(ptr);
-    return (new_ptr);
+	free(ptr);
+	return (new_ptr);
 }

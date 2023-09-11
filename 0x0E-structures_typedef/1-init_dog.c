fe513174c5ca62401e5b226986ff21462743c160
Mutai Kelvin <Mutai05>
2023-09-11 14:48:27 -0700

    Fix Betty coding style

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,21 @@
 #include "dog.h"
 #include <stddef.h>
 
-
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: pointer to the struct dog variable
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog variable
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * Return: nothing
+ * Return: None (void)
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL) /* check if d is a valid pointer */
-        return;
-    d->name = name; /* assign name to the name field of d */
-    d->age = age; /* assign age to the age field of d */
-    d->owner = owner; /* assign owner to the owner field of d */
+	if (d == NULL) /* Check if d is a valid pointer */
+		return;
+
+	d->name = name; /* Assign name to the name field of d */
+	d->age = age;   /* Assign age to the age field of d */
+	d->owner = owner; /* Assign owner to the owner field of d */
 }

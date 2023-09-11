291819d5d29f8c22e66c300462eb3adbb124a8c9
Mutai Kelvin <Mutai05>
2023-09-11 15:19:13 -0700

    Write a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees the memory allocated for a dog structure
+ * @d: Pointer to the dog structure to be freed
+ *
+ * Return: None (void)
+ */
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    {
+        /* Free the dynamically allocated name and owner strings */
+        free(d->name);
+        free(d->owner);
+
+        /* Free the dog structure itself */
+        free(d);
+    }
+}

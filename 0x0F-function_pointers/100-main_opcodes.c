310f455732dc3d924a9d9221daa25f1871dad14a
Mutai Kelvin <Mutai05>
2023-09-13 14:43:13 -0700

    Fix betty coding style

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,36 +5,39 @@
  * main - Entry point of the program
  * @argc: Number of arguments
  * @argv: Array of argument strings
- * Return: 0 on success, 1 on argument count error, 2 on negative byte count error
+ * Return: 0 on success,
+ * 1 on argument count error,
+ * 2 on negative byte count error
  */
+
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
+	int num_bytes, i;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    num_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 
-    unsigned char *main_ptr = (unsigned char *)main;
+	unsigned char *main_ptr = (unsigned char *)main;
 
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x", main_ptr[i]);
-        if (i < num_bytes - 1)
-            printf(" ");
-        else
-            printf("\n");
-    }
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
-    return (0);
+	return (0);
 }

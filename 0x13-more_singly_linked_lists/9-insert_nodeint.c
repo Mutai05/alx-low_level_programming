213be33cc474eed12c85cb12498301b986376a46
Mutai Kelvin <Mutai05>
2023-09-26 12:07:50 -0700

    Fixed Betty coding style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,43 +11,43 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *current;
-    unsigned int count = 0;
+	listint_t *new_node, *current;
+	unsigned int count = 0;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Set the value of the new node */
-    new_node->n = n;
+	/* Set the value of the new node */
+	new_node->n = n;
 
-    /* If the index is 0, set the new node as the new head */
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	/* If the index is 0, set the new node as the new head */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    current = *head;
-    /* Traverse the list to find the node at index - 1 */
-    while (current != NULL && count < idx - 1)
-    {
-        current = current->next;
-        count++;
-    }
+	current = *head;
+	/* Traverse the list to find the node at index - 1 */
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
 
-    /* If index is out of range, free the new node and return NULL */
-    if (current == NULL || count < idx - 1)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	/* If index is out of range, free the new node and return NULL */
+	if (current == NULL || count < idx - 1)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    /* Insert the new node at the specified index */
-    new_node->next = current->next;
-    current->next = new_node;
+	/* Insert the new node at the specified index */
+	new_node->next = current->next;
+	current->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }

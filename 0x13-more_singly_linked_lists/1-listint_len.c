9126194ae6c0ac5f38d7120a67a8dcae586a207c
Mutai Kelvin <Mutai05>
2023-09-26 10:29:25 -0700

    Fixed Betty coding style

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        node_count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
 
-    return (node_count);
+	return (node_count);
 }

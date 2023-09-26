1cbbfb4bec7d6b6224aa3423fa8819577fc4e84a
Mutai Kelvin <Mutai05>
2023-09-26 08:52:17 -0700

    Updated lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,13 +10,14 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
 size_t print_listint(const listint_t *h);
+
 
 #endif /* LISTS_H */

03c872cf23da8077bf7377329a4db947fdc2febe
Mutai Kelvin <Mutai05>
2023-09-28 13:41:47 -0700

    Write a function that prints the binary representation of a number. Prototype: void print_binary(unsigned long int n); File: 1-print_binary.c

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, the character written. On error, -1.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}

68c97d80bcbe34e20d3a2e8962b7722048ad049b
Mutai Kelvin <Mutai05>
2023-09-28 13:30:36 -0700

    Fixed Betty coding style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	unsigned int result = 0;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    while (*b)
-    {
-        if (*b != '0' && *b != '1')
-            return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
 
-        result = (result << 1) | (*b - '0');
-        b++;
-    }
+		result = (result << 1) | (*b - '0');
+		b++;
+	}
 
-    return (result);
+	return (result);
 }

4ca23bb089b6ba73a77907d5a11bd9e0b573d22e
Mutai Kelvin <Mutai05>
2023-10-02 14:25:17 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,43 +4,40 @@
 #include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
-    int file_descriptor;
+	ssize_t o, r, w;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	free(buffer);
+	close(o);
 
-    free(buffer);
-    close(file_descriptor);
-    return (bytes_written);
+	return (w);
 }
+

5046cbddb1537064f07803689366a2d32be7fb8d
Mutai05 <kelvok2014@gmail.com>
2023-11-13 12:50:26 -0800

    Task 1 - Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory.

--- 0x18-dynamic_libraries/libdynamic.c
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -1,41 +1,63 @@
 #include "main.h"
+#include <unistd.h>
 
 int _putchar(char c) {
     return write(1, &c, 1);
 }
 
 int _islower(int c) {
-    return (c >= 'a' && c <= 'z');
 }
 
 int _isalpha(int c) {
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 int _abs(int n) {
-    return (n < 0) ? -n : n;
 }
 
 int _isupper(int c) {
-    return (c >= 'A' && c <= 'Z');
 }
 
 int _isdigit(int c) {
-    return (c >= '0' && c <= '9');
 }
 
 int _strlen(char *s) {
-    int len = 0;
-    while (*s != '\0') {
-        len++;
-        s++;
-    }
-    return len;
 }
 
 void _puts(char *s) {
-    while (*s != '\0') {
-        _putchar(*s);
-        s++;
-    }
+}
+
+char *_strcpy(char *dest, char *src) {
+}
+
+int _atoi(char *s) {
+}
+
+char *_strcat(char *dest, char *src) {
+}
+
+char *_strncat(char *dest, char *src, int n) {
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+}
+
+int _strcmp(char *s1, char *s2) {
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+}
+
+char *_strchr(char *s, char c) {
+}
+
+unsigned int _strspn(char *s, char *accept) {
+}
+
+char *_strpbrk(char *s, char *accept) {
+}
+
+char *_strstr(char *haystack, char *needle) {
 }

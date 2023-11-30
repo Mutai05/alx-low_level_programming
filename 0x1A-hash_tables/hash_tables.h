f79db6f650ffb2ec00ff4e0b7437fb02759455f8
Mutai05 <kelvok2014@gmail.com>
2023-11-30 12:58:47 -0800

    Updated Task 7 code files

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -49,4 +49,8 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
+/* Function prototypes */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+
 #endif /* HASH_TABLES_H */

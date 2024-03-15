2fc54f8212be37abf082539b16dd2db5ee5b2366
Mutai05 <kelvok2014@gmail.com>
2024-03-15 07:25:37 -0700

    Fixed betty coding style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,45 +2,45 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array of integers using
- *                 the Binary search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for
+ * binary_search - searches for a value in an integer array using a binary
+ * search algorithm, not guaranteed to return lowest index if `value` appears
+ * twice in `array`
+ * @array: pointer to first element of array to seach
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * Return: Index where value is located, or -1 if not found
+ * Return: index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	int low, mid, high;
+	int x;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    while (left <= right)
-    {
-        size_t i;
+	low = 0;
+	high = size - 1;
 
-        printf("Searching in array:");
-        for (i = left; i <= right; i++)
-        {
-            if (i != left)
-                printf(",");
-            printf(" %d", array[i]);
-        }
-        printf("\n");
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
 
-        mid = (left + right) / 2;
+		printf("Searching in array: ");
+		for (x = low; x <= high; x++)
+			printf("%i%s", array[x], x == high ? "\n" : ", ");
 
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
 
-    return -1;
+	return (-1);
 }
